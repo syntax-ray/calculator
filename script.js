c67cf877ab6c1e2c8d33f66f1be33741700f646f
Donald Ray <syntaxray@gmail.com>
2025-06-15 10:11:54 +0100

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(num_1, num_2, oper) {
+    switch (oper) {
+        case "+":
+            return add(num_1, num_2);
+        case "-":
+            return subtract(num_1, num_2);
+        case "*":
+            return multiply(num_1, num_2);
+        case "/":
+            return divide(num_1, num_2);   
+    }
+}
